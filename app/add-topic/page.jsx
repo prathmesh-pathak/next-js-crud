e12f7b6fdda8df190cacdb88d92fe689df307ea5
Prathmesh Pathak <34978373+prathmesh-pathak@users.noreply.github.com>
2024-02-13 18:31:07 -0500

    NCRD-5 Added get and post integration with UI

--- app/add-topic/page.jsx
+++ app/add-topic/page.jsx
@@ -1,18 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title || !description) {
+      alert("Title and Description are required!");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        header: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+        alert(`Topic ${title} added successfully.`);
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console("Error occured while adding ", error);
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-3">
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
       <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Add Topic"
       />
       <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Add Topic Description"
       />
 
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+      <button
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+        type="submit"
+      >
         Add
       </button>
     </form>
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,22 +1,39 @@
 import DeleteButton from "./DeleteButton";
 import Link from "next/link";
-import { HiPencilAlt } from "react-icons";
 
-export default function TopicsList() {
+const getTopics = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.log("Error ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+
   return (
     <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-        <div>
-          <h2 className="font-bold text-2xl">Topic Title</h2>
-          <div>Topic Description</div>
+      {topics.map((topic, index) => (
+        <div key={index} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+          <div>
+            <h2 className="font-bold text-2xl">{topic?.title}</h2>
+            <div>{topic?.description}</div>
+          </div>
+          <div className="flex gap-2">
+            <DeleteButton />
+            <Link href={`/edit-topic/${topic._id}`}>Edit</Link>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <DeleteButton />
-          <Link href={"/edit-topic/123"}>
-            Edit
-          </Link>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
